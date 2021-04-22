017d2ac7028b70a34c5581e1d283a67ea5c3a240
Christian Keusch <weitnow@gmail.com>
2021-04-22 14:59:36 +0200

    2d jumping player

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,139 +1,36 @@
 import pop from "../pop/index.js";
-const { Container, entity, math, Text, Camera } = pop;
-import Level from "./Level.js";
+const { Container, Rect } = pop;
 import Player from "./entities/Player.js";
-import Pickup from "./entities/Pickup.js";
-import Bat from "./entities/Bat.js";
-import Totem from "./entities/Totem.js";
-import Ghost from "./entities/Ghost.js";
-import State from "../pop/State.js";
 
 class GameScreen extends Container {
-  constructor(game, controls, onGameOver) {
+  constructor(game, controls) {
     super();
     this.w = game.w;
     this.h = game.h;
-    this.controls = controls;
-    this.onGameOver = onGameOver;
-    const map = new Level(game.w, game.h);
-    const player = new Player(controls, map);
-    player.pos = map.findFreeSpot();
 
-    this.state = new State("READY");
-
-    this.map = this.add(map);
-    this.pickups = this.add(new Container());
-    this.player = this.add(player);
-
-    this.cam = new Camera(player, { x: 100, y: 100 });
-
-    const baddies = new Container(); // Container for enemies and Bullets
-
-    // Add a couple of Bats
-    for (let i = 0; i < 3; i++) {
-      this.randoBat(baddies.add(new Bat(player)));
-    }
-    this.baddies = this.add(baddies);
-
-    // Add a couple of Top-Hat Totems
-    for (let i = 0; i < 2; i++) {
-      const t = this.add(new Totem(player, (b) => baddies.add(b)));
-      const { x, y } = map.findFreeSpot(false); // `false` means "NOT free"
-      t.pos.x = x;
-      t.pos.y = y;
-    }
-
-    // Add a couple of Ghosts
-    for (let i = 0; i < 1; i++) {
-      const ghost = this.baddies.add(new Ghost(player, map));
-      ghost.pos = map.findFreeSpot();
-      ghost.findPath();
-    }
-
-    this.populate();
-    this.score = 0;
-    this.scoreText = this.add(
-      new Text("0", {
-        font: "40pt 'Luckiest Guy', san-serif",
-        fill: "#fff",
-        align: "center",
-      })
-    );
-    this.scoreText.pos = { x: game.w / 2, y: 180 };
-  }
-
-  populate() {
-    const { pickups, map } = this;
-    for (let i = 0; i < 5; i++) {
-      const p = pickups.add(new Pickup());
-      p.pos = map.findFreeSpot();
-    }
-  }
-
-  randoBat(bat) {
-    const { w, h } = this; // w and h of the game
-    const angle = math.randf(Math.PI * 2); // angle = random float between 0 and (pi * 2) [which is a 360 degree circle in radians]
-    bat.pos.x = Math.cos(angle) * 300 + w / 2;
-    bat.pos.y = Math.sin(angle) * 300 + h / 2;
-    bat.speed = math.rand(70, 150);
-    return bat;
+    this.bg = this.add(new Rect(this.w, this.h));
+    this.player = this.add(new Player(controls));
+    this.player.pos.x = this.w / 2 - 16;
+    this.player.pos.y = this.h / 2;
   }
 
   update(dt, t) {
-    const { controls, player, state } = this;
+    super.update(dt, t);
+    const { player, w, h } = this;
 
-    switch (state.get()) {
-      case "READY":
-        if (state.first) {
-          this.scoreText.text = "GET READY";
-        }
-        if (state.time > 2) {
-          this.scoreText.text = "0";
-          state.set("PLAYING");
-        }
-        break;
-
-      case "PLAYING":
-        super.update(dt, t);
-        this.updatePlaying();
-        break;
-
-      case "GAMEOVER":
-        if (state.first) {
-          player.gameOver = true;
-          this.scoreText.text = "DEAD. Score: " + this.score;
-        }
-        super.update(dt / 3, t); // dt / 3 = play game 3 times slower in the background
-
-        // If player dead, wait for space bar
-        if (player.gameOver && controls.action) {
-          this.onGameOver();
-        }
-        break;
+    // Check if hit ground and if so cancel gravitiy forces
+    if (player.pos.y > h / 2) {
+      player.pos.y = h / 2;
+      this.player.jumping = false;
     }
 
-    state.update(dt);
-  }
-
-  updatePlaying() {
-    const { baddies, player, pickups, state } = this;
-    baddies.map((baddie) => {
-      if (entity.hit(player, baddie)) {
-        state.set("GAMEOVER");
-        baddie.dead = true;
-      }
-    });
-
-    // Collect pickup!
-    entity.hits(player, pickups, (p) => {
-      p.dead = true;
-      this.score++;
-      if (pickups.children.length === 1) {
-        this.populate();
-        this.score += 5;
-      }
-      this.scoreText.text = this.score;
-    });
+    // Screen wrap
+    if (player.pos.x < -48) {
+      player.pos.x = w;
+    }
+    if (player.pos.x > w) {
+      player.pos.x = -48;
+    }
   }
 }
 
--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,58 +1,40 @@
 import pop from "../../pop/index.js";
-const { Texture, TileSprite, wallslide } = pop;
+const { Texture, TileSprite } = pop;
 
 const texture = new Texture("res/images/bravedigger-tiles.png");
 
 class Player extends TileSprite {
-  constructor(controls, map) {
+  constructor(controls) {
     super(texture, 48, 48); // 48, 48 = with and height of the player
     this.controls = controls;
-    this.map = map;
-    this.hitBox = {
-      x: 8,
-      y: 10,
-      w: 28,
-      h: 38,
-    };
     this.speed = 210;
-    this.anchor = { x: 0, y: 0 };
+    this.jumping = false;
+    this.vel = 0;
   }
 
   update(dt, t) {
-    const { pos, controls, gameOver, map, speed } = this;
+    const { pos, controls, speed } = this;
+    const { x } = controls; // No need for 'y'
+    const xo = x * dt * speed;
+    let yo = 0;
 
-    if (gameOver) {
-      this.rotation += dt * 5;
-      this.pivot.y = 16;
-      this.pivot.x = 16;
-      return;
+    //Apply som gravity
+    if (this.jumping) {
+      this.vel += 32 * dt;
+      yo += this.vel;
     }
 
-    let { x, y } = controls;
-    const xo = x * dt * speed;
-    const yo = y * dt * speed;
+    pos.x += xo; // horizontal influenced by the keyboard
+    pos.y += yo; // vertical influenced by "gravity"
 
-    // Can we move to this position?
-    const r = wallslide(this, map, xo, yo);
-    pos.x += r.x;
-    pos.y += r.y;
+    if (!this.jumping && controls.action) {
+      // Jump!
+      this.vel = -10; // Launch force
+      this.jumping = true;
+    }
 
-    // Animate!
-    if (xo || yo) {
-      // Walking frames
-      this.frame.x = ((t / 0.08) | 0) % 4;
-      // Walking left or right?
-      if (xo < 0) {
-        this.scale.x = -1;
-        this.anchor.x = 48;
-      }
-      if (xo > 0) {
-        this.scale.x = 1;
-        this.anchor.x = 0;
-      }
-    } else {
-      // Just hanging out
-      this.frame.x = (((t / 0.2) | 0) % 2) + 4;
+    if (x && !this.jumping) {
+      this.frame.x = ((t / 0.1) | 0) % 2;
     }
   }
 }
